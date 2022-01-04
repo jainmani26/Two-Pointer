496f50c6d5985cdd4f580ad66052343a815c4f8f
jainmani26 <93339993+jainmani26@users.noreply.github.com>
2022-01-04 19:32:10 +0530

    Create Two Sum - Less than or equal to target.java

---
+++ Two Sum - Less than or equal to target.java
@@ -0,0 +1,22 @@
+public int twoSum5(int[] nums, int target) 
+    {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        int count = 0;
+
+        while(left < right)
+        {
+            int sum = nums[left] + nums[right];
+
+            if(sum <= target)
+            {
+                count += right - left;
+                left++;
+            }
+            else
+            {
+                right --;
+            }
+        }
+        return count;
+    }
